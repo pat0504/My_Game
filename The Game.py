daaeaa3b9b91a3066a32cd2ad1c987aa3fe5a664
Patrick Deegan <pat667deegan@gmail.com>
2018-04-05 12:43:13 -0400

    The base of my game so far

---
+++ The Game.py
@@ -0,0 +1,24 @@
+#This is Patrick's text base RPG adventure.
+class Player
+    """ You are the player """
+
+    def __init__(self, name)
+        self.name = name
+        self.life = 100
+        self.level = 1
+        self.attack
+        self.defense
+        self.Hp
+        self.Xp = 0
+
+    def add_level(self, level)
+    
+    def game_over(self)
+        
+    
+    def death
+        if life >= 0
+            game_over
+        else:
+            
+        
