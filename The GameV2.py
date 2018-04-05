54bb14896d8de887925f29f08ba81b1d775f9353
Patrick Deegan <pat667deegan@gmail.com>
2018-04-05 13:56:56 -0400

    worked on adding the classes and a few other things

---
+++ The GameV2.py
@@ -0,0 +1,44 @@
+#This is Patrick's text base RPG adventure.
+class Player
+    """ You are the player """
+
+    def __init__(self, name)
+        self.name = name
+        self.life = 1
+        self.attack = attack
+        self.defense = defense
+        self.hp = 100
+        self.xp = xp
+
+    def level_up(self, level)
+        while hero.xp >= Player.level2
+        
+    def game_over(self)
+        
+    
+    def death
+        if life >= 0
+            game_over
+        else:
+
+
+class Warrior(Player):
+    def __init__(self):
+        super().__init__(name = input("What is your characters name?"), attack = 10, defense = 10
+                         hp = 100, xp = 0)
+    prof = "Warrior"
+    maxhp = 100
+    level = 1
+    ####################### This is what I was in the middle of working on it.
+def profession():
+    print("What is your class?",'\n',
+          " press W for Warrior",'\n',
+          " press M for Mage")
+    pclass=input(">>>")
+    if pclass =="W":
+        Prof = Warrior()
+    else pclass == "M":
+        Prof = Mage()
+
+        #profession()
+        
