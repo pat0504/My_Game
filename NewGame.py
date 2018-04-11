eaa2039c12eb6d4a9584f4a965e7f09e2121d60f
Patrick Deegan <pat667deegan@gmail.com>
2018-04-11 13:56:10 -0400

    finished title screen and started map

--- NewGame.py
+++ NewGame.py
@@ -29,4 +29,67 @@ def title_screen_selections():
     elif option.lower() == ("quit"):
         sys.exit()
     while option.lower() not in ['play', 'help', 'quit']:
- 
+        print("Please enter a valid command.")
+        options = input("> ")
+        if option.lower() == ("play"):
+            start_game() # placeholder until written
+        elif option.lower() == ("help"):
+            help_menu()
+        elif option.lower() == ("quit"):
+            sys.exit()
+
+def title_screen():
+    os.system('clear')
+    print('-----------------------')
+    print('| Welcome to my Game! |')
+    print('-----------------------')
+    print('        - Play -       ')
+    print('        - Help -       ')
+    print('        - Quit -       ')
+    title_screen_selections()
+
+def help_menu():
+    print('-----------------------')
+    print('| Welcome to my Game! |')
+    print('-----------------------')
+    print('- Use up, down, left, and right to move')
+    print('- Type your commands to do them')
+    print('- Good luck and have fun and do not die!')
+    title_screen_selections()
+
+#### GAME FUNCTIONALITY ####
+def start_game():
+
+
+#### VISUAL MAP ####
+"""
+ a1 a2 a3 a4
+-------------
+|  |  |  |  | a4
+-------------
+|  |  |  |  | b4
+-------------
+|  |  |  |  | c4
+-------------
+|  |  |  |  | d4
+-------------
+"""
+
+DESCRIPTION = 'description'
+EXAMINATION = 'examination'
+SOLVED = False
+UP = 'up', 'north'
+DOWN = 'down', 'south'
+LEFT = 'left', 'west'
+RIGHT = 'right', 'east'
+
+solved_places = {}
+#### stopped at vid 3 6:04 ####
+
+
+
+
+
+
+
+
